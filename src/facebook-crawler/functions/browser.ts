45a1e33b6f86bb1dca1f19836d6aa845a31d28ff
Kyung Lee <kyung.lee.official@outlook.com>
2025-05-16 11:25:22 +0800

    fix: fix browser launch options

--- src/facebook-crawler/functions/browser.ts
+++ src/facebook-crawler/functions/browser.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer, { Browser, LaunchOptions } from "puppeteer";
 
 export const launchBrowser = async (): Promise<Browser> => {
 	const enableProxy = !!(
@@ -7,15 +7,16 @@ export const launchBrowser = async (): Promise<Browser> => {
 		process.env.PROXY_PORT
 	);
 
+	let l: LaunchOptions;
+
 	try {
 		const browser: Browser = await puppeteer.launch({
 			headless: false,
-			executablePath: process.env.CHROME_EXECUTABLE_PATH,
 			userDataDir: process.env.CHROME_USER_DATA_DIR,
 			args: enableProxy
 				? [
-						`--profile-directory=${process.env.CHROME_PROFILE_NAME}`,
 						`--proxy-server=${process.env.PROXY_PROTOCOL}://${process.env.PROXY_HOST}:${process.env.PROXY_PORT}`,
+						`--profile-directory=${process.env.CHROME_PROFILE_NAME}`,
 					]
 				: [`--profile-directory=${process.env.CHROME_PROFILE_NAME}`],
 			devtools: true,
